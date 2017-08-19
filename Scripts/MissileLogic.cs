c0d8891571b9e34f19924fd8db816c023f212107
Alexander Hjelm <nick.nertova@gmail.com>
2017-08-19 17:02:12 +0200

    Init

---
+++ Scripts/MissileLogic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLogic : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {        
+        gameObject.transform.Translate(Vector3.up * -1.0f);              
+    }
+}
